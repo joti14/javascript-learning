915399bc3e794f231d7609e59040c897e1173cbd
Hasna Hena Joti <93066126+joti14@users.noreply.github.com>
2024-12-20 15:13:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Joti"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+// object
+
+console.log(typeof null) // obeject
+console.log(typeof undefined) // undefined
