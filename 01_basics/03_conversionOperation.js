147c0146bc9e29df7a1fffe1177d4e67b08a43a0
Hasna Hena Joti <93066126+joti14@users.noreply.github.com>
2024-12-21 09:01:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Joti"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "23" => 23
+// "23sfg" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Joti"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Joti" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
