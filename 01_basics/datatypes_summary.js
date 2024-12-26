1343629a81ee436127e9f6889f59fdb077548dce
Hasna Hena Joti <93066126+joti14@users.noreply.github.com>
2024-12-26 13:10:20 +0000

    comparison

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // "undefined" value
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 34532425398475798357n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Funtions
+
+const heros = ["Spiderman", "Batman", "Superman"]
+
+let myObj = {
+    name: "Joti",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+myFunction()
+
+console.log(typeof myFunction)
