bbf34f1062e71d7585bbc5a17748f1213094908a
Hasna Hena Joti <93066126+joti14@users.noreply.github.com>
2024-12-31 06:01:10 +0000

    primitive and non-primitive

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,37 +1,67 @@
-// Primitive
+// // Primitive
 
-// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.4
+// const score = 100
+// const scoreValue = 100.4
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail; // "undefined" value
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail; // "undefined" value
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
-const bigNumber = 34532425398475798357n
+// const bigNumber = 34532425398475798357n
 
-// Reference (Non Primitive)
+// // Reference (Non Primitive)
 
-// Array, Objects, Funtions
+// // Array, Objects, Funtions
 
-const heros = ["Spiderman", "Batman", "Superman"]
+// const heros = ["Spiderman", "Batman", "Superman"]
 
-let myObj = {
-    name: "Joti",
-    age: 24,
+// let myObj = {
+//     name: "Joti",
+//     age: 24,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world")
+// }
+
+// myFunction()
+
+// console.log(typeof myFunction)
+// console.log(typeof outsideTemp)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive Types) and Heap (Non-Premitive Types)
+// 
+
+let myChannelname = "hasnahena" // value directly stored
+
+let anothername = myChannelname // creates a complete copy
+anothername = "jotipoti" // Doesnt affect myChannelname
+
+console.log(myChannelname)
+console.log(anothername);
+
+// Heap
+
+// Reference stored
+let userOne = {
+    email: "user@gmail.com",
+    username: "rohan"
 }
 
-const myFunction = function(){
-    console.log("Hello world")
-}
+let userTwo = userOne // Points to same memory reference
 
-myFunction()
+userTwo.email = "rohan@gmail.com" // changes both userOne 
+                                   //and userTwo
 
-console.log(typeof myFunction)
-console.log(typeof outsideTemp)
+console.log(userOne.email);
+console.log(userTwo.email);
