ac7e3c5f0ec6ab3a9f7f2059cd48bae97cfd16a0
Hasna Hena Joti <93066126+joti14@users.noreply.github.com>
2025-01-10 14:28:38 +0000

    objects part one

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Hasnahena",
+    age: 25,
+    [mySym]: "mykey1",
+    location: "Cumilla",
+    email: "joti@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Friday"]
+}
+
+// accessing object elements
+// console.log(JsUser.email);
+
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "joti@google.com" //changing values
+// Object.freeze(JsUser) // values cant be changed now
+// JsUser.email = "joti@microsoft.com" 
+
+// console.log(JsUser);
+
+
+JsUser.greetings = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingsTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greetings());
+console.log(JsUser.greetingsTwo());
